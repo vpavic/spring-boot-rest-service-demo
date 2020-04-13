d122a80a61bf5b8978eb0772456369687aeb5701
Vedran Pavic <vedran.pavic@gmail.com>
2020-04-13 22:11:09 +0200

    Upgrade dependencies

--- src/main/java/demo/interfaces/product/ProductResource.java
+++ src/main/java/demo/interfaces/product/ProductResource.java
@@ -1,10 +1,11 @@
 package demo.interfaces.product;
 
 import demo.domain.model.product.Product;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.hateoas.RepresentationModel;
 import org.springframework.hateoas.server.core.Relation;
 
-@Relation(collectionRelation = "item")
+@Relation(collectionRelation = IanaLinkRelations.ITEM_VALUE)
 class ProductResource extends RepresentationModel<ProductResource> {
 
     private final String name;
